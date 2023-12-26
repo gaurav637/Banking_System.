0fc0f7f6e6165a864d428069938f91155fb367b9
GauravNegi <negigaurav637@gmail.com>
2023-12-26 20:14:51 +0530

    Add new commit

--- src/BankSystem/Bank_Management.java
+++ src/BankSystem/Bank_Management.java
@@ -11,6 +11,8 @@ public class Bank_Management {
     private static final String user = "root";
     private static final String password = "91491026";
     public static void main(String args[])throws ClassNotFoundException {
+        String email;
+        int account_number;
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("driver is loaded..");
@@ -23,22 +25,71 @@ public class Bank_Management {
                 Scanner sc = new Scanner(System.in);
                 User user = new User(conn,sc);
                 Accounts ac = new Accounts(conn,sc);
+                AccountManage am = new AccountManage(conn,sc);
                 System.out.println("BANK MANAGEMENT SYSTEM");
                 System.out.println("1.register");
                 System.out.println("2. login");
                 System.out.println("3. exit");
                 System.out.println("choice any option -> ");
                 int n = sc.nextInt();
-                String ans;
                 switch(n){
                     case 1:{
                         user.Register();
                         break;
                     }
                     case 2:{
-                        ans = user.login();
-                        if(ans!=null){
+                        email = user.login();
+                        if(email!=null){
+                            System.out.println();
                             System.out.println("Account is login-> ");
+                            if(!ac.accounts_exists(email)){
+                                System.out.println();
+                                System.out.println("1. open new bank account ");
+                                System.out.println("2. Exists");
+                                int n1 = sc.nextInt();
+                                if(n1==1){
+                                    int account_no = ac.open_accounts(email);
+                                    System.out.println("your account is created..");
+                                    System.out.println("your account number is -> "+ account_no);
+                                }
+                                else{
+                                    break;
+                                }
+                            }
+                            account_number = ac.grtAccountsNumber(email);
+                            int choice = 0;
+                            while(choice!=5){
+                                System.out.println();
+                                System.out.println("credit money");
+                                System.out.println("debit money");
+                                System.out.println("transferred money ");
+                                System.out.println("Log Out");
+                                System.out.println();
+                                System.out.println("Enter your choise -> ");
+                                choice = sc.nextInt();
+                                switch(choice){
+                                    case 1:{
+                                        am.debit_money(account_number);
+                                        break;
+                                    }
+                                    case 2:{
+                                        am.credit_money(account_number);
+                                        break;
+                                    }
+                                    case 3:{
+                                        am.transfer_money(account_number);
+                                        break;
+                                    }
+                                    case 4:{
+                                        am.transfer_money(account_number);
+                                        break;
+                                    }
+                                    case 5:{
+                                        break;
+                                    }
+                                }
+                            }
+
                         }
                         break;
                     }
